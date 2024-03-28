13e13711ed1a602b3152c5171d9ede3958693a08
Futuristic Programmer© <81916900+123452123-codr@users.noreply.github.com>
2024-03-28 22:31:21 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,12 @@
-import tkinter
+import tkinter as tk
+from tkinter import *
 
-m = tkinter.Tk()
+window = Tk()
+Label(window, text='Username:').grid(row=0)
+Label(window, text='Password:').grid(row=1)
+e1 = Entry(window)
+e2 = Entry(window)
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
 
-
-m.mainloop()
+mainloop()
